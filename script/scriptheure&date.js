6b0ca74525e2b87dc86bd2e99057238593dfe12f
baayvin17 <125447478+baayvin17@users.noreply.github.com>
2023-04-19 11:01:22 +0200

    fin

--- script/scriptheure&date.js
+++
@@ -1,6 +0,0 @@
-// afficher la date et l'heure
-setInterval(() => {
-    const date = new Date();
-    document.getElementById("date").textContent = date.toLocaleDateString();
-    document.getElementById("time").textContent = date.toLocaleTimeString();
-}, 1000);
